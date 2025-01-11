fba9049484208c49ce32c77407aea18a32d404c4
Juan408AR <aguirre.juanmp15@gmail.com>
2025-01-10 19:28:06 -0800

    Initial Commit

---
+++ Road Guard Watch App/Sources/App/Road_GuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Road_GuardApp.swift
+//  Road Guard Watch App
+//
+//  Created by Juan R Aguirre on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Road_Guard_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
