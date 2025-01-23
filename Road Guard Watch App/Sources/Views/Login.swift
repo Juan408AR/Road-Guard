1632d2cf370a814252cad8d8317fa1724b9c07e0
Juan408AR <aguirre.juanmp15@gmail.com>
2025-01-22 23:33:04 -0800

    Saving current progress. Working on styling login page.

--- Road Guard Watch App/Sources/Views/Login.swift
+++ Road Guard Watch App/Sources/Views/Login.swift
@@ -13,18 +13,40 @@ struct LoginPage: View {
     @State private var showingAlert: Bool = false
     
     var body: some View {
-        VStack(spacing: 10){
-            Text("Login")
-                .font(.system(size: 24, weight: .bold))
+        ZStack {
+            VStack(){
+                
+                Spacer()
+                    .frame(height: 50)
+                TextField("Email", text: $email)
+                    .textContentType(.emailAddress)
+                    .textInputAutocapitalization(.never)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
+                
+                
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
+                
+                Button(action: {
+                    showingAlert = true
+                }) {
+                    Text("Login")
+                        .font(.headline)
+                        .foregroundStyle(Color.white)
+                        .padding()
+                        .background(Color.blue.opacity(0.6))
+                        .cornerRadius(10)
+                }
                 .padding()
-            
-            TextField("Email", text: $email)
-                .textContentType(.emailAddress)
-                .textInputAutocapitalization(.never)
-                .padding(8)
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(8)
-                .padding(.horizontal)
+                Spacer()
+            }
         }
             
     }
